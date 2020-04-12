3c4b1f2c7a2ef706f43705dbef28c9e097852aa5
Libertas3301 <57844736+Libertas3301@users.noreply.github.com>
2020-04-12 17:42:15 +0300

    few changes

---
+++ src/assembler.ts
@@ -0,0 +1,7 @@
+import { AppService } from './app.service';
+import { AppController } from './app.controller';
+
+const appService = new AppService();
+const appController = new AppController(appService);
+const hello = appController.getHello();
+console.log(hello);
